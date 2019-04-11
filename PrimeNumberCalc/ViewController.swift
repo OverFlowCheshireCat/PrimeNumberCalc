7eb7a08744c90b94e7fd506c67e2ca545a7da779
dit03 <dit@imac-4.local>
2019-04-11 15:47:52 +0900

    New Source

--- PrimeNumberCalc/ViewController.swift
+++ PrimeNumberCalc/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var numberInput: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        numberInput.delegate = self
+    }
+    
+    @IBAction func checkButton(_ sender: Any) {
+        var check = true
+        if let number = Int(numberInput.text!) {
+        
+            if number == 0 || number == 1 {
+                check = false
+            }
+            
+            if check {
+                var i = 2
+                
+                while i < number {
+                    if number % i == 0 {
+                        check = false
+                    }
+                    i += 1
+                }
+            }
+            
+            if check {
+                resultLabel.text = String(number) + " 은 소수가 맞습니다."
+            } else {
+                if number == 0 {
+                    resultLabel.text = "올바른 숫자를 입력하세요 !"
+                } else {
+                    resultLabel.text = String(number) + " 은 소수가 아닙니다."
+                }
+            }
+        } else {
+            resultLabel.text = "올바른 숫자를 입력하세요 !"
+        }
+        
+        numberInput.text = ""
+        numberInput.resignFirstResponder()
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        numberInput.resignFirstResponder()
+        
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
